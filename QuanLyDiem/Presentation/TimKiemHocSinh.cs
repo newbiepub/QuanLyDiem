22d61ef2815bd6040dc3f51344f957033af17880
nguyenvunamphuc <namphuc.96@gmail.com>
2017-05-09 14:28:33 +0700

    Tìm Kiem vs Database

--- QuanLyDiem/Presentation/TimKiemHocSinh.cs
+++ QuanLyDiem/Presentation/TimKiemHocSinh.cs
@@ -18,8 +18,9 @@ namespace QuanLyDiem.Presentation
             this.radioButton1.Checked = true;
             this.groupBasic.Show();
             this.groupAdvance.Hide();
-            studentGrade.DropDownStyle = ComboBoxStyle.DropDownList;
-        }
+           
+        }  
+        Data_Layer.DataAccess da = new Data_Layer.DataAccess();
 
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
@@ -35,25 +36,64 @@ namespace QuanLyDiem.Presentation
                 {
                     this.groupAdvance.Show();
                     this.groupBasic.Hide();
+                    ResetValue_Click(null, null);
                 }
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Data_Layer.DataAccess da = new Data_Layer.DataAccess();
+          
+            string theDate = date.Value.ToString("dd/MM/yyyy");
+        // Check textbox is null
+            // TenHocSinh
+            string tenHocSinh = "";
+           
+           if(studentName.Text !="")
+           {
+               tenHocSinh = "and TenHocSinh ='" + studentName.Text + "'";
+           }
+           // MaHocSinh
+           string maHocSinh = "";
+
+           if (studentCodeAdvance.Text != "0")
+           {
+               maHocSinh = "and MaHocSinh ='" + studentCodeAdvance.Text + "'";
+           }
+           // NgaySinh
+           string ngaySinh = "";
+
+           if (dateSwitch.IsOn == true)
+           {
+               ngaySinh = "and NgaySinh ='" + theDate + "'";
+           }
+           // TenLop
+           string tenLop = "";
+
+           if (studentClass.Text != "Không")
+           {
+               tenLop = "and LOP.TenLop ='" + studentClass.Text + "'";
+           }
+           // Khoi
+           string khoi = "";
+
+           if (studentGrade.Text != "Không")
+           {
+               khoi = "and LOP.khoi = '" + studentGrade.Text + "'";
+           }
+           string query = "";
             if(radioButton1.Checked)
             {
 
-                string query = "SELECT * FROM HOC_SINH WHERE MaHocSinh ='" + txtStudentCodeBasic.Text + "';";
-                dataGridView1.DataSource = da.select(query);
+                query = "SELECT MaHocSinh,TenHocSinh,TenLop,NgaySinh,DiaChi, GioiTinh,Khoi,GhiChu,NamHoc, HOC_SINH.MaLop FROM HOC_SINH,LOP WHERE HOC_SINH.MaLop = LOP.Malop and MaHocSinh = '"+txtStudentCodeBasic.Value+"'";
             }
             else
             {
-                string theDate = date.Value.ToString("dd/MM/yyyy");
-                string query = "SELECT * FROM HOC_SINH,LOP WHERE HOC_SINH.MaLop = LOP.Malop and (NgaySinh ='" + theDate + "' or TenHocSinh ='" + studentName.Text + "' or LOP.TenLop ='" + studentClass.Text + "' or MaHocSinh ='" + studentCodeAdvance.Text + "' or LOP.khoi = '"+studentGrade.Text+"') ;";
-                dataGridView1.DataSource = da.select(query);
+
+                query = "SELECT MaHocSinh,TenHocSinh,TenLop,NgaySinh,DiaChi, GioiTinh,Khoi,GhiChu,NamHoc, HOC_SINH.MaLop FROM HOC_SINH,LOP WHERE HOC_SINH.MaLop = LOP.Malop " + ngaySinh + " " + khoi + " " + maHocSinh + " " + tenHocSinh + " " + tenLop + ";";
+
             }
+            dataGridView1.DataSource = da.select(query);
             if(dataGridView1.RowCount == 1)
             {
                 MessageBox.Show("Không có học sinh !!");
@@ -68,6 +108,48 @@ namespace QuanLyDiem.Presentation
             }
         }
 
+   
+
+        private void dateSwitch_Toggled(object sender, EventArgs e)
+        {
+            if (dateSwitch.IsOn == true)
+            {
+                date.Enabled = true;
+            }
+            else
+            {
+                date.Enabled = false;
+            }
+        }
+
+        private void ResetValue_Click(object sender, EventArgs e)
+        {
+            studentClass.Text = "Không";
+            studentGrade.Text = "Không";
+            studentName.Text = "";
+            txtStudentCodeBasic.Value = 0;
+            studentCodeAdvance.Value = 0;
+            dateSwitch.IsOn = false;
+        }
+
+        private void TimKiemHocSinh_Load(object sender, EventArgs e)
+        {
+           
+            // TODO: This line of code loads data into the 'lop.LOP' table. You can move, or remove it, as needed.
+           
+           // this.lOPTableAdapter.Fill(this.lop.LOP);
+            string query = "select TenLop from LOP";
+            DataTable _dt = da.select(query);
+            _dt.Rows.Add("Không");
+            studentClass.DataSource = _dt;
+            studentClass.DisplayMember = "TenLop";
+           
+           
+            
+            
+        }
+
+      
       
 
      
