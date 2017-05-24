c91077ea477f914cb78aab4616588724272502fc
LamNguyen <lamnh@lungvang.vn>
2017-05-24 15:51:14 +0700

    Update Controller

---
+++ QuanLyDiem/Controller/Khoi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.DataAccess.Native.Data;
+using QuanLyDiem.Business_Logic;
+using DataTable = System.Data.DataTable;
+
+namespace QuanLyDiem.Controller
+{
+    class Khoi
+    {
+        public static DataTable getAllKhoi()
+        {
+            KhoiBL khoi = new KhoiBL();
+            return khoi.getAllKhoi();
+        }
+    }
+}
---
+++ QuanLyDiem/Controller/Lop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.DataAccess.Native.Data;
+using QuanLyDiem.Business_Logic;
+using DataTable = System.Data.DataTable;
+
+namespace QuanLyDiem.Controller
+{
+    class Lop
+    {
+        public static DataTable getLopByKhoi(int khoi, string nienkhoa)
+        {
+            LopBL bl = new LopBL(khoi, nienkhoa);
+            return bl.getLopByKhoi();
+        }
+
+        public static DataTable getLop()
+        {
+            LopBL bl = new LopBL();
+            return bl.getLop();
+        }
+
+        public static DataTable getNienKhoa()
+        {
+            LopBL bl = new LopBL();
+            return bl.getNienKhoa();
+        }
+     }
+}
